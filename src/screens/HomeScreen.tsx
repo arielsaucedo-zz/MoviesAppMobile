90dd7a5aa47228fe9140231fa933885f146b991d
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-06-23 16:31:39 -0300

    Creacion de poster de peliculas

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import {ActivityIndicator, Text, View} from 'react-native';
+
 import {useMovies} from '../hooks/useMovies';
+
+import {MoviePoster} from '../components/MoviePoster';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
   const {peliculasEnCine, isLoading} = useMovies();
+  const {top} = useSafeAreaInsets();
 
   if (isLoading) {
-    <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
-      <ActivityIndicator color="red" size={100} />
-    </View>;
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+        <ActivityIndicator color="red" size={100} />
+      </View>
+    );
   }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View
+      style={{
+        marginTop: top + 20,
+      }}>
+      <MoviePoster movie={peliculasEnCine[0]} />
     </View>
   );
 };
